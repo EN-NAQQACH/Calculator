4e746e419d6a579d9bfac96c7478c2ea58645f2b
ENNAQQACH <mohssineennaqqach0@gmail.com>
2023-07-22 16:09:01 +0100

    Calculator

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -4,8 +4,6 @@ export default function Header(props){
     return(
         <div className="header">
             <p>{props.valueofbtn}</p>
-            <p>{props.result}</p>
-            
         </div>
     )
 }
